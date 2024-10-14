eff1a8bfe851361f347f2840b11a3432d3f07979
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-10-14 16:49:50 -0500

    Create connection to db postgres and add dockercompose

---
+++ db/connection.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const DSN = "host=localhost user=admin password=YWRtaW5pc3RyYXRvcgo= dbname=dbadmin port=5432"
+
+var DB *gorm.DB
+
+func DBconnection() {
+	var err error
+	DB, err = gorm.Open(postgres.Open(DSN), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("DB is runig")
+	}
+}
--- main.go
+++ main.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/JuanPerdomo00/go-restAPI/db"
+	"github.com/JuanPerdomo00/go-restAPI/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	const PORT = "8080"
+	db.DBconnection()
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World desde go"))
-	})
+	r.HandleFunc("/", routes.HomeHandler)
 
 	fmt.Printf("Server on http://127.0.0.1:%s\n", PORT)
 	http.ListenAndServe(":8080", r)
