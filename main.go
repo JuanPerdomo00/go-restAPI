26acee6850b5beba5fc131bffb420e54d154d5e5
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-10-15 20:07:03 -0500

    Update: Add function of muxr

--- main.go
+++ main.go
@@ -6,23 +6,18 @@ import (
 
 	"github.com/JuanPerdomo00/go-restAPI/db"
 	"github.com/JuanPerdomo00/go-restAPI/models"
-	"github.com/JuanPerdomo00/go-restAPI/routes"
+	muxrouters "github.com/JuanPerdomo00/go-restAPI/mux-routers" // muxrouters as
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewRouter()
 	const PORT = "8080"
 	db.DBconnection()
 	db.DB.AutoMigrate(models.Task{})
 	db.DB.AutoMigrate(models.User{})
-	r := mux.NewRouter()
 
-	r.HandleFunc("/", routes.HomeHandler)
-
-	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
-	r.HandleFunc("/user/{id}", routes.GetUserHandler).Methods("GET")
-	r.HandleFunc("/user/{id}", routes.PostUserHandler).Methods("POST")
-	r.HandleFunc("/user/{id}", routes.DeleteUserHandler).Methods("DELETE")
+	muxrouters.HandlerUserMain() // Handlers Users
 
 	log.Printf("Server on http://127.0.0.1:%s\n", PORT)
 	http.ListenAndServe(":8080", r)
