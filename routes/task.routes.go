a36242ea23a291462e9179eaf1703ce469e6a271
Juan Perdomo [Jakepy] <perdomojuan187@gmail.com>
2024-10-15 20:07:29 -0500

    Update: Add empty function task

--- routes/task.routes.go
+++ routes/task.routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import "net/http"
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+
+}
